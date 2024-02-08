941e4e8a451347a368c3438fae690d46da7785aa
Wisdom Okogho <wisdommokoghoo@gmail.com>
2024-02-08 02:46:25 +0100

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+module.exports = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
 const nextConfig = {
   images: {
     remotePatterns: [
